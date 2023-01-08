f14b09945b477381a1b29d12b902919391eba88d
gil9red <ilya.petrash@inbox.ru>
2023-01-08 17:58:07 +0500

    Рефакторинг

--- generate_games/generate_games.py
+++ generate_games/generate_games.py
@@ -86,77 +86,82 @@ def remove_partial_duplicates(genres: list[str], need_log: bool = True) -> list[
     return genres
 
 
-log.info('Start.')
+def run():
+    log.info('Start.')
 
-if FILE_NAME_GAMES.exists():
-    backup_file_name = str(
-        FILE_NAME_BACKUP / f'{get_current_datetime_str()}_{FILE_NAME_GAMES.name}'
-    )
-    shutil.copy(
-        FILE_NAME_GAMES,
-        backup_file_name
-    )
-    log.info(f'Save backup to: {backup_file_name}')
+    if FILE_NAME_GAMES.exists():
+        backup_file_name = str(
+            FILE_NAME_BACKUP / f'{get_current_datetime_str()}_{FILE_NAME_GAMES.name}'
+        )
+        shutil.copy(
+            FILE_NAME_GAMES,
+            backup_file_name
+        )
+        log.info(f'Save backup to: {backup_file_name}')
+        log.info('')
+
+    log.info('Loading cache...')
+
+    game_by_genres = load(FILE_NAME_GAMES)
+    log.info(f'game_by_genres ({len(game_by_genres)})')
+
+    new_game_by_genres = Dump.dump()
+    log.info(f'new_game_by_genres ({len(new_game_by_genres)})')
+
+    genre_translate = load()
+    log.info(f'genre_translate ({len(genre_translate)})')
+
+    log.info('Finish loading cache.')
     log.info('')
 
-log.info('Loading cache...')
+    log.info('Search games...')
 
-game_by_genres = load(FILE_NAME_GAMES)
-log.info(f'game_by_genres ({len(game_by_genres)})')
+    number = 0
 
-new_game_by_genres = Dump.dump()
-log.info(f'new_game_by_genres ({len(new_game_by_genres)})')
-
-genre_translate = load()
-log.info(f'genre_translate ({len(genre_translate)})')
-
-log.info('Finish loading cache.')
-log.info('')
-
-log.info('Search games...')
-
-number = 0
-
-for game, genres in new_game_by_genres.items():
-    if game in game_by_genres:
-        continue
-
-    log.info(f'Added game {game!r} with genres ({len(genres)}): {genres}')
-    number += 1
-
-    new_genres = []
-
-    for x in genres:
-        tr_genres = genre_translate.get(x)
-        if not tr_genres:  # null, [], ""
+    for game, genres in new_game_by_genres.items():
+        if game in game_by_genres:
             continue
 
-        if isinstance(tr_genres, str):
-            new_genres.append(tr_genres)
+        log.info(f'Added game {game!r} with genres ({len(genres)}): {genres}')
+        number += 1
 
-        elif isinstance(tr_genres, list):
-            new_genres.extend(tr_genres)
+        new_genres = []
 
-        else:
-            log.warning(f'Unsupported type genres {tr_genres} from {x!r}')
+        for x in genres:
+            tr_genres = genre_translate.get(x)
+            if not tr_genres:  # null, [], ""
+                continue
 
-    new_genres = do_genres_compression(new_genres)
-    new_genres = remove_partial_duplicates(new_genres)
+            if isinstance(tr_genres, str):
+                new_genres.append(tr_genres)
 
-    log.info(f'Successful translate genres ({len(new_genres)}): {new_genres}')
-    game_by_genres[game] = new_genres
+            elif isinstance(tr_genres, list):
+                new_genres.extend(tr_genres)
 
-    log.info('')
+            else:
+                log.warning(f'Unsupported type genres {tr_genres} from {x!r}')
 
-log.info(f'Finish search games. New games: {number}.')
+        new_genres = do_genres_compression(new_genres)
+        new_genres = remove_partial_duplicates(new_genres)
 
-log.info(f'Saving to {FILE_NAME_GAMES}')
+        log.info(f'Successful translate genres ({len(new_genres)}): {new_genres}')
+        game_by_genres[game] = new_genres
 
-json.dump(
-    game_by_genres,
-    open(FILE_NAME_GAMES, 'w', encoding='utf-8'),
-    ensure_ascii=False,
-    indent=4
-)
+        log.info('')
 
-log.info('Finish!')
+    log.info(f'Finish search games. New games: {number}.')
+
+    log.info(f'Saving to {FILE_NAME_GAMES}')
+
+    json.dump(
+        game_by_genres,
+        open(FILE_NAME_GAMES, 'w', encoding='utf-8'),
+        ensure_ascii=False,
+        indent=4
+    )
+
+    log.info('Finish!')
+
+
+if __name__ == '__main__':
+    run()
