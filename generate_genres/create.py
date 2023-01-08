02dfd74b8d46aa52e576deeea62abe3f5c8ba511
gil9red <ilya.petrash@inbox.ru>
2023-01-08 20:41:47 +0500

    Добавлен скрипт генерации жанров

---
+++ generate_genres/create.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from pathlib import Path
+
+from common import load_json, save_json
+from common_utils import get_logger
+from genre_translate_file.load import FILE_NAME_GENRE_TRANSLATE
+
+
+FILE_NAME_GENRES = str(Path(__file__).parent.resolve() / 'genres.json')
+
+
+log = get_logger('generate_genres.txt')
+
+
+def run():
+    log.info("Старт")
+
+    current_genres: dict = load_json(FILE_NAME_GENRES)
+    log.info(f'Жанров: {len(current_genres)}')
+
+    genre_translate: dict = load_json(FILE_NAME_GENRE_TRANSLATE)
+
+    all_genres = set()
+    for genres in genre_translate.values():
+        if not genres:
+            continue
+
+        if isinstance(genres, list):
+            all_genres.update(genres)
+        else:
+            all_genres.add(genres)
+
+    all_genres = sorted(all_genres)
+    log.info(f'Жанров из файла трансляции: {len(all_genres)}')
+
+    new_genres = dict()
+    for genre in all_genres:
+        genre_info = current_genres.get(genre)
+        if genre_info:
+            new_genres[genre] = genre_info
+        else:
+            log.info(f'Добавлен новый жанр: {genre!r}')
+            new_genres[genre] = dict(
+                title='',
+                description='',
+            )
+
+    number = len(new_genres) - len(current_genres)
+    if number:
+        log.info(f'Добавлено жанров: {number}')
+
+        log.info(f'Сохранение в {FILE_NAME_GENRES}')
+        save_json(new_genres, FILE_NAME_GENRES)
+    else:
+        log.info('Новых жанров нет. Сохранять не нужно')
+
+    log.info("Финиш\n")
+
+
+if __name__ == '__main__':
+    run()
