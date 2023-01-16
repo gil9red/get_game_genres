ad11a7579d223fca7922dda7e30c7a617b0deb91
gil9red <ilya.petrash@inbox.ru>
2023-01-16 06:41:42 +0500

    Рефакторинг

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ import time
 from timeit import default_timer
 from threading import Thread
 
+from config import IGNORE_SITE_NAMES
+from common import get_logger, get_games_list
 from db import db_create_backup, Dump
-from common import IGNORE_SITE_NAMES
-from common_utils import get_parsers, get_games_list, get_logger, print_parsers
 from generate_games import generate_games as create_generate_games
 from generate_genres import create as create_generate_genres
 from genre_translate_file import create as create_genre_translate
+from parsers import get_parsers, print_parsers
 from parsers.base_parser import BaseParser
 from third_party.atomic_counter import AtomicCounter
 from third_party.seconds_to_str import seconds_to_str
