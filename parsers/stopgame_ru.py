1d0fef3d1a3f22d164dae141b2632e741bb923b1
gil9red <ilya.petrash@inbox.ru>
2025-07-07 00:42:05 +0500

    Исправление ошибки. parsers/stopgame_ru.py

--- parsers/stopgame_ru.py
+++ parsers/stopgame_ru.py
@@ -5,29 +5,29 @@ __author__ = "ipetrash"
 
 
 import re
+from urllib.parse import urljoin
+
 from parsers.base_parser import BaseParser
 
 
 class StopgameRuParser(BaseParser):
     def _parse(self) -> list[str]:
-        url = f"https://stopgame.ru/search/?s={self.game_name}&where=games&sort=relevance"
-        root = self.send_get(url, return_html=True)
+        url = f"https://stopgame.ru/ajax/search/games/?term={self.game_name}&offset=0&sort=relevance"
+        data: dict = self.send_get(url, return_json=True)
 
-        for game_block in root.select("div[data-key]"):
-            title = self.get_norm_text(game_block.select_one('h2[class*="_title_"]'))
-            if not self.is_found_game(title):
+        for game in filter(lambda obj: obj["type"] == "game", data["results"]):
+            if not self.is_found_game(game["title"]):
                 continue
 
-            genre_title_el = game_block.select_one(
-                '*[class*="_info-grid__title_"]:-soup-contains("Жанр")'
-            )
-            genre_value_el = genre_title_el.find_next_sibling(
-                attrs={"class": re.compile("info-grid__value_")}
-            )
+            url_game = urljoin(url, game["url"])
+
+            self.log_debug(f"Загрузка {url_game!r}")
+            soup = self.send_get(url_game, return_html=True)
 
             # Сойдет первый, совпадающий по имени, вариант
             return [
-                self.get_norm_text(a) for a in genre_value_el.select('a')
+                self.get_norm_text(a)
+                for a in soup.select('a[class *= "_tag_"]')
             ]
 
         self.log_info(f"Not found game {self.game_name!r}")
@@ -44,16 +44,16 @@ if __name__ == "__main__":
     _common_test(get_game_genres)
 
     # Search 'Hellgate: London'...
-    #     Genres: ['action', 'rpg']
+    #     Genres: ['Мультиплеер', 'Научная фантастика', 'Одиночная', 'От первого лица', 'От третьего лица', 'Ролевая', 'Хоррор', 'Шутер', 'Экшн']
     #
     # Search 'The Incredible Adventures of Van Helsing'...
-    #     Genres: ['rpg']
+    #     Genres: ["Hack & Slash/Beat 'em up", 'Кооперативная', 'Мультиплеер', 'Одиночная', 'Ролевая', 'Сверху/Изометрия', 'Фэнтези', 'Экшн']
     #
     # Search 'Dark Souls: Prepare to Die Edition'...
     #     Genres: []
     #
     # Search 'Twin Sector'...
-    #     Genres: ['action', 'logic']
+    #     Genres: ['Головоломка', 'Научная фантастика', 'Одиночная', 'От первого лица', 'Приключение', 'Шутер', 'Экшн']
     #
     # Search 'Call of Cthulhu: Dark Corners of the Earth'...
-    #     Genres: ['action', 'adventure']
+    #     Genres: ['Выживание', 'Детектив/Тайна', 'Одиночная', 'От первого лица', 'Приключение', 'Стелс', 'Хоррор', 'Шутер', 'Экшн']
