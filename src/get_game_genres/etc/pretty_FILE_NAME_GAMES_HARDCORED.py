e1b6b928564895c9d12c8ddf489a52022ee9dd04
gil9red <ilya.petrash@inbox.ru>
2026-04-18 03:22:39 +0500

    Обновлено. get_game_genres/etc/pretty_FILE_NAME_GAMES_HARDCORED.py

--- src/get_game_genres/etc/pretty_FILE_NAME_GAMES_HARDCORED.py
+++ src/get_game_genres/etc/pretty_FILE_NAME_GAMES_HARDCORED.py
@@ -9,7 +9,8 @@ from get_game_genres.common import load_json, save_json
 
 
 data: dict = load_json(FILE_NAME_GAMES_HARDCORED)
-new_data: dict = {
-    k: data[k] for k in sorted(data)
-}
-save_json(new_data, FILE_NAME_GAMES_HARDCORED)
+if data:
+    new_data: dict = {
+        k: data[k] for k in sorted(data)
+    }
+    save_json(new_data, FILE_NAME_GAMES_HARDCORED)
