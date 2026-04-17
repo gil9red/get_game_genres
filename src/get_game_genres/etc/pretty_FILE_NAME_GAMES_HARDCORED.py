538496799f7b3f9e4d0b864e84dd809c2a92f255
gil9red <ilya.petrash@inbox.ru>
2026-04-18 02:42:28 +0500

    Добавлено. get_game_genres/etc/pretty_FILE_NAME_GAMES_HARDCORED.py

---
+++ src/get_game_genres/etc/pretty_FILE_NAME_GAMES_HARDCORED.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+from get_game_genres.generate_games.generate_games import FILE_NAME_GAMES_HARDCORED
+from get_game_genres.common import load_json, save_json
+
+
+data: dict = load_json(FILE_NAME_GAMES_HARDCORED)
+new_data: dict = {
+    k: data[k] for k in sorted(data)
+}
+save_json(new_data, FILE_NAME_GAMES_HARDCORED)
